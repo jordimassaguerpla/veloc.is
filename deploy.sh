ec6adb10b65140719b03ad5e9026debbb632edfe
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-02-01 12:48:13 +0100

    add version to the path and add instructions

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ set -e
 
 
 rm -rf .build || true
-mkdir -p .build/nextcloud/apps
+mkdir -p .build/nextcloud/apps/v1
 cp index.html .build/
-wget -c https://apps.nextcloud.com/api/v1/categories.json -O .build/nextcloud/apps/categories.json
-wget -c https://apps.nextcloud.com/api/v1/apps.json -O .build/nextcloud/apps/apps.json
+wget -c https://apps.nextcloud.com/api/v1/categories.json -O .build/nextcloud/apps/v1/categories.json
+wget -c https://apps.nextcloud.com/api/v1/apps.json -O .build/nextcloud/apps/v1/apps.json
 aws s3 sync .build s3://www.veloc.is/
