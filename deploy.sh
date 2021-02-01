1ef18edf721d89e93757b446d6169292d39bd922
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2021-02-01 13:03:47 +0100

    add sync date
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ set -e
 rm -rf .build || true
 mkdir -p .build/nextcloud/apps/v1
 cp index.html .build/
+sed -e "s%__DATE_NC_SYNC_%$(date)%g" -i .build/index.html
 wget -c https://apps.nextcloud.com/api/v1/categories.json -O .build/nextcloud/apps/v1/categories.json
 wget -c https://apps.nextcloud.com/api/v1/apps.json -O .build/nextcloud/apps/v1/apps.json
 aws s3 sync .build s3://www.veloc.is/
